1ac3934d4c3e3a75cb2aa54e74a4636cfa7fedf6
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-17 19:24:57 +0200

    Tema 3 los 2 primeros ej con fallo en el 2

--- UD3/02_custom_filter/main.js
+++ UD3/02_custom_filter/main.js
@@ -1,6 +1,38 @@
 const assert = require('assert').strict;
 
 function select(array, condition) {
+    // `${condition}(${array})`
+    return condition(array)
+}
+
+function pairs(array){
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]%2 !=0){
+            array.splice(i,1)
+        }
+    }
+    console.log(array)
+    return array
+   }
+
+function gt15(array){
+    for (let i = 0; i < array.length; i++) {
+        if(array[i]<15){
+            array.splice(i)
+        }
+    }
+    console.log(array)
+    return array
+}
+
+function lt10(array){
+    for (let i = 0; i < array.length; i++) {
+        if(array[i]>10){
+            array.splice(i)
+        }
+    }
+    console.log(array)
+    return array
 }
 
 let values = [1, 2, 3, 5, 7, 13, 17, 23, 29]
